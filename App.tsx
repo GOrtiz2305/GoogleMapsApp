7b9a04ebad067c0b3eb9e0e1ca8f1cdd3d47e740
GOrtiz2305 <160190916+GOrtiz2305@users.noreply.github.com>
2024-11-08 14:28:17 -0600

    App

--- App.tsx
+++ App.tsx
@@ -1,117 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+function App(): React.JSX.Element {
+  const [latitude, setLatitude] = useState(48.86948);
+  const [longitude, setLongitude] = useState(2.78137);
+  const [region, setRegion] = useState({
+    latitude: 48.86948,
+    longitude: 2.78137,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  const newLocation = () => {
+    setRegion({
+      ...region,
+      latitude: latitude,
+      longitude: longitude,
+    });
+  };
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <MapView style={styles.map} region={region}>
+        <Marker coordinate={{ latitude: latitude, longitude: longitude }} />
+      </MapView>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          value={latitude.toString()}
+          onChangeText={(text) => {
+            // Permite sólo números, punto y el signo negativo al principio
+            const formattedText = text.replace(/[^0-9.-]/g, '');
+            setLatitude(parseFloat(formattedText));
+          }}
+          placeholder="Latitud"
+          keyboardType="numeric"
+        />
+        <TextInput
+          style={styles.input}
+          value={longitude.toString()}
+          onChangeText={(text) => {
+            const formattedText = text.replace(/[^0-9.-]/g, '');
+            setLongitude(parseFloat(formattedText));
+          }}
+          placeholder="Longitud"
+          keyboardType="numeric"
+        />
+        <Button title="Buscar" onPress={newLocation} />
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  map: {
+    width: '100%',
+    height: '90%',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  inputContainer: {
+    padding: 10,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    width: '40%',
+    marginRight: 5,
   },
 });
 
